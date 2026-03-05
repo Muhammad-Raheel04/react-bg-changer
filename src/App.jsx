7909b9c96d4fcfc2de2a857d81acfeb1e6440b5a
Muhammad Raheel <raheel31102004@gmail.com>
2026-03-05 17:13:03 +0500

    implemented use state to dynamically change bg color

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [color, setColor] = useState('violet');
+  const colors = [
+    { name: "Red", value: "red" },
+    { name: "Green", value: "green" },
+    { name: "Blue", value: "blue" },
+    { name: "Violet", value: "violet" },
+    { name: "Orange", value: "orange" },
+    { name: "Yellow", value: 'yellow' }
+  ]
   return (
-    <div className=''>
-      bg-changer
+    <div className='h-screen w-full duration-200' style={{ backgroundColor: color }}>
+      <h1 className='fixed flex flex-wrap justify-center inset-x-0 text-3xl p-2'>Background Changer App</h1>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white p-3 rounded-xl'>
+          {
+            colors.map((c) =>
+              <button
+                key={c.name}
+                onClick={() => setColor(c.value)}
+                className='outline-none px-4 border'
+                style={{ backgroundColor: c.value }}
+              >
+                {c.name}
+              </button>
+            )
+          }
+        </div>
+      </div>
     </div>
   )
 }
